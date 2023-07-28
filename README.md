f65794ae0313964bd0581d44abe2eddf11851d0b
DoormatIka <trashcanartistlily@gmail.com>
2023-07-28 16:31:33 +0900

    done

---
+++ README.md
@@ -0,0 +1,13 @@
+# Comfortable Video Downloader
+GUI incoming soon!
+
+## Installation
+- Install node.js v18+
+- Run `npm install` on the directory.
+
+## Usage
+`links.txt` is where you put Soundcloud, YouTube, whatever links.
+
+Run the script with `npx ts-node --esm index.ts`
+
+The videos will be installed inside the `"completed/NA"` folder. Playlists will be installed inside a `"completed/{playlist name}"` folder.
--- index.ts
+++ index.ts
@@ -1,21 +1,33 @@
 import ytdl from "youtube-dl-exec";
 import logger from "progress-estimator";
-import fs from "fs";
+import { createReadStream } from "fs";
+import split2 from "split2";
+import internal from "stream";
 
-async function createDownloadSession(link: string) {  
+const LINK_PATH = "./links.txt"
+const EXTRACT_AUDIO = true;
+
+async function createDownloadSession(link: string) {
     const progress = logger({}); // progress bar initialization
     const res = ytdl(link, {
         retries: 3,
         continue: true,
         configLocation: "./",
+        extractAudio: EXTRACT_AUDIO
     });
-    const progress_bar_print = await progress(res, `Completing ${link}`);
-    console.log(progress_bar_print);
+    const progress_bar_print = await progress(res, `Completing ${link}.`);
+    console.log(`\n${progress_bar_print}`);
+}
+async function parser(links: internal.Transform) {
+    for await (const link of links) {
+        await createDownloadSession(link);
+    }
 }
 
-async function parser(links: fs.ReadStream) {
-    
-}
-
-// createDownloadSession(test.soundcloud)
-createDownloadSession("https://www.youtube.com/watch?v=IKXDyKWXBhk");
+const file = createReadStream(LINK_PATH, { encoding: "utf8" });
+await parser(
+    file
+        .pipe(split2())
+        .on("close", function() { file.destroy() })
+);
+file.destroy();
